8cc7d00771fdc3656d5933d055aa2c148709bb51
Krystalllll <jrjyyzy@gmail.com>
2016-12-13 22:23:11 -0500

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python3
+
+import os
+import string
 import subprocess
 import sys
+import re
 
 
-# YOUR CODE GOES here
+indent = '│   '
+space = '    '
+branch = '├── '
+child_branch = '└── '
+
+def sort_key(s):
+    return re.sub('[^A-Za-z0-9]+', '', s).lower()
+
+def print_tree(cpath, padding):
+    files = [file for file in os.listdir(cpath) if not file.startswith('.')]
+    files = sorted(files, key=sort_key)
+    num_dir = 0
+    num_file = 0
+    for index in range(len(files)):
+        file = files[index]
+        if index < len(files) - 1:
+            next_padding = indent
+            print(padding + branch + file)
+        else:
+            print(padding + child_branch + file)
+            next_padding = space
+        if os.path.isfile(os.path.join(cpath, file)):
+            num_file += 1
+        else:
+            num_dir += 1
+            f_dir_num, f_file_num = print_tree(os.path.join(cpath, file), padding + next_padding)
+            num_dir += f_dir_num
+            num_file += f_file_num
+    return num_file, num_dir
 
 
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    if len(sys.argv) == 1:
+        print(".")
+        pathway = os.getcwd() + '/'
+    # elif len(sys.argv) == 2:
+    else:
+        temway = sys.argv[1]
+        if temway[0] != '/':
+            pathway = os.getcwd() + '/' + temway + '/'
+        else:
+            pathway = temway + '/'
+        print(temway)
+    num_dir, num_file = print_tree(pathway, "")
+    print('\n' + str(num_dir) + " directories, " + str(num_file) + " files")
